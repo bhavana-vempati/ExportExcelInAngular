8b021ae14022623edcd33569d7e63414cf60f976
bhavana-vempati <bhavanavempati455@gmail.com>
2020-10-12 18:08:07 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,71 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { FormBuilder, FormGroup } from "@angular/forms";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
-export class AppComponent {
-  title = 'ExcelInAngular';
+export class AppComponent implements OnInit {
+  @ViewChild("fileUploader", { static: false }) fileUploader: ElementRef;
+  @ViewChild("labelImport", { static: false }) labelImport: ElementRef;
+
+  fileToUploadOnSelect: File = null;
+  myuseridstring = "1";
+  uploadForm: FormGroup;
+  beforePost: boolean = true;
+  onPost: boolean = false;
+  fileadded: any;
+
+  SERVER_URL = "http://localhost:3000/upload";
+  constructor(
+    private httpClient: HttpClient,
+    private formBuilder: FormBuilder
+  ) {}
+
+  ngOnInit() {
+    this.uploadForm = this.formBuilder.group({
+      profile: [""],
+    });
+  }
+  onFileChange(files: FileList) {
+    if (files.length > 0) {
+      this.labelImport.nativeElement.innerText = Array.from(files)
+        .map((f) => f.name)
+        .join(", ");
+      this.fileToUploadOnSelect = files.item(0);
+    }
+  }
+
+  resetFileUploader() {
+    this.labelImport.nativeElement.innerText = "Choose file";
+    this.fileUploader.nativeElement.value = "";
+    this.fileToUploadOnSelect = null;
+  }
+  onSubmit() {
+    if (this.fileToUploadOnSelect != null) {
+      this.beforePost = false;
+      this.onPost = true;
+      const formData = new FormData();
+      formData.append(
+        "file",
+        this.fileToUploadOnSelect,
+        this.fileToUploadOnSelect.name
+      );
+      //any id should be converted to form data
+      formData.append("userId", this.myuseridstring);
+
+      this.httpClient.post<any>("this.SERVER_URL", formData).subscribe(
+        (data) => {
+          if (data.success == true) {
+            //return api data
+          }
+        },
+        (error) => {}
+      );
+    } else {
+      alert("please add file before you post");
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import {
+  HttpClientModule /* other http imports */,
+} from "@angular/common/http";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
